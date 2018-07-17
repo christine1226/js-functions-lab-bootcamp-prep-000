48b259ea9267400da7bf2880f5398bee93665264
christine.gomez1226@gmail.com <christine.gomez1226@gmail.com>
2018-07-17 01:09:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function happyHolidays(){
 
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`}
+
+function happyHolidayTo(holiday, name)
+{return `Happy ${holiday}, ${name}!`}
+
+function holidayCountdown(holiday, days)
+{returns `It's ${days} days until ${holiday}!`}
